6f805c224b596c8ab7e5130e9c41499c1986fd0f
Yerassyl <75775516+moooge89@users.noreply.github.com>
2022-05-15 16:40:01 +0600

    feat: get products with wishlist and cart

--- MelonAPI/Repository/impl/ProductRepository.cs
+++ MelonAPI/Repository/impl/ProductRepository.cs
@@ -17,7 +17,11 @@ namespace MelonAPI.Repository.impl
         
         public List<ProductLight> LoadProductByCategoryId(int CategoryId, int userId)
         {
-            string query = $"select * from product where category_id = {CategoryId}";
+            string query = @$"select p.id as id, p.name as name, p.price as price, p.category_id as category_id,
+                           (select exists(select * from wishlist w where w.product_id = p.id and w.user_id = {userId})) as is_wishlist,
+                           (select exists(select * from cart where cart.product_id = p.id and cart.user_id = {userId})) as is_cart
+                           from product p
+                           where category_id = {CategoryId};";
 
             DataTable dataTable = new();
             string sqlDataSource = configuration.GetConnectionString("MelonAppCon");
@@ -45,8 +49,8 @@ namespace MelonAPI.Repository.impl
                     Name = row.Field<string>("name"),
                     Price = row.Field<decimal>("price"),
                     CategoryId = row.Field<int>("category_id"),
-                    IsInCart = false,
-                    IsInWishlist = false,
+                    IsInCart = row.Field<bool>("is_cart"),
+                    IsInWishlist = row.Field<bool>("is_wishlist"),
                 };
                 products.Add(product);
             }
@@ -57,9 +61,13 @@ namespace MelonAPI.Repository.impl
         public Product LoadProductById(int productId, int userId)
         {
             string query = @$"select p.id as product_id, p.name as product_name,
-                            p.description, p.price, p.count, p.manufacturer,
-                            p.category_id, c.id as category_id, c.name as category_name
-                            from product p, category c where p.category_id = c.id and p.id = {productId};";
+                              p.description, p.price, p.count, p.manufacturer,
+                              p.category_id, c.id as category_id, c.name as category_name,
+                              (select exists (select * from wishlist w where w.product_id = p.id and w.user_id = {userId})) as is_wishlist,
+                              (select exists (select * from cart where cart.product_id = p.id and cart.user_id = {userId})) as is_cart
+                              from product p, category c
+                              where p.category_id = c.id
+                              and p.id = {productId};";
 
             DataTable dataTable = new();
             string sqlDataSource = configuration.GetConnectionString("MelonAppCon");
@@ -90,6 +98,8 @@ namespace MelonAPI.Repository.impl
                 Price = dataTable.Rows[0].Field<decimal>("price"),
                 Count = dataTable.Rows[0].Field<int>("count"),
                 Manufacturer = dataTable.Rows[0].Field<string>("manufacturer"),
+                IsInWishlist = dataTable.Rows[0].Field<bool>("is_wishlist"),
+                IsInCart = dataTable.Rows[0].Field<bool>("is_cart"),
             };
 
             Category category = new()
